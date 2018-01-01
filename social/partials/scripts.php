4ce294b18bbe831217f3b2db54c3713ac295dcd1
Norbert Gotowczyc <norbert.gotowczyc@gmail.com>
2018-01-01 19:42:04 +0100

    Make scroll to top button look better
    Icon will change to better when I'll buy font awesome v5 pro.

--- social/partials/scripts.php
+++ social/partials/scripts.php
@@ -1,6 +1,6 @@
 <!-- Put the scroll button here because why not -->
 <div id="button-scroll-top" style="display: none;">
-    <i class="d-block fa fa-arrow-circle-o-up" aria-hidden="true"></i>
+    <i class="d-block fa fa-arrow-circle-up" aria-hidden="true"></i>
 </div>
 
 <script type="text/javascript" src="../resources/scripts/lib/jquery/3.2.1/jquery.min.js"></script>
