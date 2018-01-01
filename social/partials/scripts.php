a6a99d5a9fb73f581298b0b5c3bc6f9794c6bb96
Norbert Gotowczyc <norbert.gotowczyc@gmail.com>
2018-01-01 08:13:58 +0100

    Use the new version of main scripts file instead of the cached one

--- social/partials/scripts.php
+++ social/partials/scripts.php
@@ -6,4 +6,4 @@
 <script type="text/javascript" src="../resources/scripts/lib/jquery/3.2.1/jquery.min.js"></script>
 <script type="text/javascript" src="../resources/scripts/lib/popper.js/1.13.0/popper.min.js"></script>
 <script type="text/javascript" src="../resources/scripts/lib/bootstrap/4.0.0-beta.3/bootstrap.min.js"></script>
-<script type="text/javascript" src="../resources/scripts/core.js"></script>
+<script type="text/javascript" src="../resources/scripts/core.js?v=<?php echo $websiteVersion['commit']; ?>"></script>
