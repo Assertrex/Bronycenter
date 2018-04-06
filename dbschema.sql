412b53a3b93d76e51103e85b1f043c7aca380177
Norbert Gotowczyc <norbert.gotowczyc@gmail.com>
2018-04-07 01:11:23 +0200

    [DB] DBSchema for previous commit, as I've forgot.
    Tables will no longer be dropped on using this SQL in case that you forget that you had a database before. I don't know, it's just for a first setup anyway.
    
    Also you should use this SQL in case if those two fields exists in your database.
    
    ALTER TABLE `users` DROP IF EXISTS `displayname_change_datetime`, DROP IF EXISTS `displayname_change_reversed`;

--- dbschema.sql
+++ dbschema.sql
@@ -9,7 +9,6 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 
-DROP TABLE IF EXISTS `key_email`;
 CREATE TABLE IF NOT EXISTS `key_email` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` int(10) UNSIGNED NOT NULL,
@@ -22,7 +21,6 @@ CREATE TABLE IF NOT EXISTS `key_email` (
   UNIQUE KEY `hash` (`hash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `log_logins`;
 CREATE TABLE IF NOT EXISTS `log_logins` (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` int(10) UNSIGNED NOT NULL,
@@ -32,17 +30,15 @@ CREATE TABLE IF NOT EXISTS `log_logins` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `messages_simple_conversations`;
 CREATE TABLE IF NOT EXISTS `messages_simple_conversations` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_one_id` int(10) UNSIGNED NOT NULL,
   `user_two_id` int(10) UNSIGNED NOT NULL,
-  `last_message_datetime` datetime NOT NULL,
+  `last_message_datetime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `messages_count` int(10) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `messages_simple_messages`;
 CREATE TABLE IF NOT EXISTS `messages_simple_messages` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `conversation_id` int(10) UNSIGNED NOT NULL,
@@ -53,27 +49,25 @@ CREATE TABLE IF NOT EXISTS `messages_simple_messages` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `posts`;
 CREATE TABLE IF NOT EXISTS `posts` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` int(10) UNSIGNED NOT NULL,
-  `ip` varchar(45) DEFAULT NULL,
   `datetime` datetime NOT NULL,
   `content` text DEFAULT NULL,
-  `like_count` mediumint(8) UNSIGNED NOT NULL DEFAULT 0,
-  `comment_count` mediumint(8) UNSIGNED NOT NULL DEFAULT 0,
+  `like_count` smallint(5) UNSIGNED NOT NULL DEFAULT 0,
+  `comment_count` smallint(5) UNSIGNED NOT NULL DEFAULT 0,
   `edit_count` smallint(5) UNSIGNED NOT NULL DEFAULT 0,
+  `report_count` smallint(5) UNSIGNED NOT NULL DEFAULT 0,
+  `report_checked` tinyint(1) NOT NULL DEFAULT 0,
   `type` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
   `status` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
   `delete_moderator` tinyint(1) NOT NULL DEFAULT 0,
   `delete_id` int(10) UNSIGNED DEFAULT NULL,
-  `delete_ip` varchar(45) DEFAULT NULL,
   `delete_datetime` datetime DEFAULT NULL,
   `delete_reason` tinytext DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `posts_comments`;
 CREATE TABLE IF NOT EXISTS `posts_comments` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `post_id` int(10) UNSIGNED NOT NULL,
@@ -84,7 +78,6 @@ CREATE TABLE IF NOT EXISTS `posts_comments` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `posts_edits`;
 CREATE TABLE IF NOT EXISTS `posts_edits` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `post_id` int(10) UNSIGNED NOT NULL,
@@ -97,7 +90,6 @@ CREATE TABLE IF NOT EXISTS `posts_edits` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `posts_likes`;
 CREATE TABLE IF NOT EXISTS `posts_likes` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `post_id` int(10) UNSIGNED NOT NULL,
@@ -106,19 +98,16 @@ CREATE TABLE IF NOT EXISTS `posts_likes` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `posts_reported`;
 CREATE TABLE IF NOT EXISTS `posts_reported` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `post_id` int(10) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
-  `category` tinyint(3) UNSIGNED NOT NULL,
+  `category` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
   `reason` tinytext DEFAULT NULL,
   `datetime` datetime NOT NULL,
-  `result` tinyint(3) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `display_name` varchar(32) NOT NULL,
@@ -138,8 +127,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `account_standing` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
   `suggestion_points` smallint(5) UNSIGNED NOT NULL DEFAULT 0,
   `displayname_changes` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
-  `displayname_change_datetime` datetime DEFAULT NULL,
-  `displayname_change_reversed` tinyint(1) NOT NULL DEFAULT 0,
   `displaynames_recent` varchar(162) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
@@ -148,7 +135,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `avatar` (`avatar`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `users_details`;
 CREATE TABLE IF NOT EXISTS `users_details` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` int(10) UNSIGNED NOT NULL,
@@ -169,19 +155,18 @@ CREATE TABLE IF NOT EXISTS `users_details` (
   UNIQUE KEY `userId` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `users_statistics`;
 CREATE TABLE IF NOT EXISTS `users_statistics` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` int(10) UNSIGNED NOT NULL,
   `user_points` int(10) NOT NULL DEFAULT 0,
   `posts_created` mediumint(8) UNSIGNED NOT NULL DEFAULT 0,
+  `posts_likes_given` mediumint(8) UNSIGNED NOT NULL DEFAULT 0,
+  `posts_comments_given` mediumint(8) UNSIGNED NOT NULL DEFAULT 0,
   `posts_removed` mediumint(8) UNSIGNED NOT NULL DEFAULT 0,
   `posts_removed_mod` mediumint(8) UNSIGNED NOT NULL DEFAULT 0,
-  `posts_likes_given` mediumint(8) UNSIGNED NOT NULL DEFAULT 0,
   `posts_likes_received` mediumint(8) UNSIGNED NOT NULL DEFAULT 0,
   `posts_comments_removed` mediumint(8) UNSIGNED NOT NULL DEFAULT 0,
   `posts_comments_removed_mod` mediumint(8) UNSIGNED NOT NULL DEFAULT 0,
-  `posts_comments_given` mediumint(8) UNSIGNED NOT NULL DEFAULT 0,
   `posts_comments_received` mediumint(8) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id` (`user_id`)
