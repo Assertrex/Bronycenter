9c7ecb8f5ae94aec7877392d2066eeab3a9f0023
Norbert Gotowczyc <norbert.gotowczyc@gmail.com>
2018-07-16 23:09:39 +0200

    Re-use configuration code for both Web and CLI applications.
    Don't repeat settings code, as same are used by both public/index.php and cli/doctrine.php.

--- cli/config/bootstrap.php
+++ cli/config/bootstrap.php
@@ -7,45 +7,15 @@ use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Doctrine\ORM\Tools\Setup;
 use Slim\Container;
 
-// Include Composer's autoloader
-require_once '../vendor/autoload.php';
-
-// Turn on PHP errors reporting
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
-// Load website settings (from .env file)
-$dotEnv = new Dotenv\Dotenv('..');
-$dotEnv->load();
-
-// Set a timezone for PHP to use (from .env file)
-date_default_timezone_set($_ENV['PHP_DEFAULT_TIMEZONE']);
+// Include PHP file containing re-usable configuration code
+require_once(__DIR__ . '/../../application/config/bootstrap.php');
 
 // Create a Slim container with defined settings (from .env file)
 $container = new Container([
-    'settings' => [
-        'displayErrorDetails' => true,
-        'determineRouteBeforeAppMiddleware' => false,
-        'doctrine' => [
-            'dev_mode' => true,
-            'cache_dir' => '../cache/doctrine',
-            'metadata_dirs' => [
-                '../application/models'
-            ],
-            'connection' => [
-                'driver' => (!empty($_ENV['DOCTRINE_DB_DRIVER'])) ? $_ENV['DOCTRINE_DB_DRIVER'] : 'pdo_mysql',
-                'host' => (!empty($_ENV['DOCTRINE_DB_HOSTNAME'])) ? $_ENV['DOCTRINE_DB_HOSTNAME'] : 'localhost',
-                'port' => (!empty(intval($_ENV['DOCTRINE_DB_PORT']))) ? $_ENV['DOCTRINE_DB_PORT'] : 3306,
-                'dbname' => $_ENV['DOCTRINE_DB_DATABASE'],
-                'user' => $_ENV['DOCTRINE_DB_USERNAME'],
-                'password' => $_ENV['DOCTRINE_DB_PASSWORD'],
-                'charset' => (!empty($_ENV['DOCTRINE_DB_CHARSET'])) ? $_ENV['DOCTRINE_DB_CHARSET'] : 'utf8mb4'
-            ]
-        ]
-    ]
+    'settings' => $slimSettings
 ]);
 
+// Create an EntityManager for Doctrine ORM
 $container[EntityManager::class] = function (Container $container): EntityManager {
     $config = Setup::createAnnotationMetadataConfiguration(
         $container['settings']['doctrine']['metadata_dirs'],
--- cli/doctrine.php
+++ cli/doctrine.php
@@ -3,8 +3,10 @@
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
-$container = require_once 'config/bootstrap.php';
+// Include PHP file containing CLI configuration code
+$container = require_once('config/bootstrap.php');
 
+// Run a CLI Doctrine application
 ConsoleRunner::run(
     ConsoleRunner::createHelperSet($container[EntityManager::class])
 );
